fee9719600781bc72c5e71b90c68ff50bb9bee7a
sofja.kulickowa <sofja.kulickowa@yandex.ru>
2026-03-11 22:06:54 +0300

    chore: API and require tymon/jwt-auth have installed
    - AuthController has created: login, logout, refresh, me methotds have added;
    - User`s model have updated: new interface and methods have added;
    - JWT and API packages have added;

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/logout', 'logout');
+    Route::post('/refresh', 'refresh');
+    Route::post('/me', 'me');
+});
